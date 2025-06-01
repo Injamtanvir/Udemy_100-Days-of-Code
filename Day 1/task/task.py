a5e9de6fdd88607229183eb90dddcc536630a0d2
INJAM UL HAQUE <injamtanvir@gmail.com>
2025-06-01 20:33:25 +0600

    Day1

---
+++ Day 1/task/task.py
@@ -0,0 +1,7 @@
+# InjamTanvir
+
+print("Welcome to the Band Name Generator")
+city_name = input("What is your name?\n")
+pet_name = input("What is your name?\n")
+
+print("Your band name could be " + city_name + " " + pet_name)
